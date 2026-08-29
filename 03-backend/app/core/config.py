48fd344284d6ded072ca51d034b100ad35699a11
MD.KHALEEL-UR-RAHMAN <mdkhaleelurrahman51@gmail.com>
2026-08-29 09:36:57 +0530

    deploy: publish DEVOS live demo

--- 03-backend/app/core/config.py
+++ 03-backend/app/core/config.py
@@ -29,7 +29,7 @@ class Settings(BaseSettings):
     def assemble_cors_origins(cls, v: str | list[str]) -> str | list[str]:
         if isinstance(v, str) and not v.startswith("["):
             return [i.strip() for i in v.split(",")]
-        elif isinstance(v, (list, str)):
+        elif isinstance(v, list):
             return v
         raise ValueError(v)
 
